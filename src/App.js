728aa30e870076610490c76146bf6a8f4a5c2759
byohannes <42332389+byohannes@users.noreply.github.com>
2020-05-26 15:43:24 +0100

    Basic page with comonents done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import babyNamesData from './babyNamesData.json';
+import ListBabyNames from "./components/ListBabyNames";
+import "./App.css";
 const App = () => {
-  return  (<div>
-      Hello baby Name Picker
-  </div>);
+  return  (
+      <div className="container">
+    <input className="search-input" placeholder="Search baby names..."></input> 
+    <hr className="horizontal-line"></hr>
+    <ListBabyNames babyNames = {babyNamesData}/>
+    <hr className="horizontal-line"></hr>
+    </div>
+  );
 }
 export default App;
