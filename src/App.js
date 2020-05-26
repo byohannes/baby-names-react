a88da08ba2f0f46e71cef5938e5fb575b4fb7a4f
byohannes <42332389+byohannes@users.noreply.github.com>
2020-05-26 17:45:55 +0100

    updated with opyion buttons for genders

--- src/App.js
+++ src/App.js
@@ -6,9 +6,13 @@ import './App.css';
 const App = () => {
   const [names, setName] = useState ('');
   const [babyName, setBabyName] = useState ([]);
+  const [genderOption, setGenderOption] = useState ('');
 
-  const handleChange = (e) => {
+  const searchName = e => {
     setName (e.target.value.toLowerCase ());
+  };
+  const filterByGender = e => {
+    setGenderOption (e.target.value);
   };
 
   useEffect (
@@ -16,18 +20,39 @@ const App = () => {
       setBabyName (
         babyNamesData.filter (item => item.name.toLowerCase ().includes (names))
       );
+      setGenderOption (
+        babyNamesData.filter (item => item.sex.includes (genderOption))
+      );
     },
-    [names]
+    [names, genderOption]
   );
   return (
     <div className="container">
       <input
         className="search-input"
         placeholder="Search baby names..."
-        onChange={handleChange}
+        onChange={searchName}
       />
-      <img className="gender-image" src={image} alt="gender"/>
-      <hr className="horizontal-line"/>
+      <div className="radio-buttons">
+        <span class="options">
+          <input
+            type="radio"
+            name="gender"
+            value="m"
+            onChange={filterByGender}
+          />
+          <label for="male" className="gender">Male</label>
+          <input
+            type="radio"
+            name="gender"
+            value="f"
+            onChange={filterByGender}
+          />
+          <label for="female" className="gender">Female</label>
+        </span>
+      </div>
+      <img className="gender-image" src={image} alt="gender" />
+      <hr className="horizontal-line" />
       <ListBabyNames babyNames={babyName} />
       <hr className="horizontal-line" />
     </div>
