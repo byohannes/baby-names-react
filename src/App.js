814102f97c9d385e9409d99a99d4c7805fd845d6
byohannes <42332389+byohannes@users.noreply.github.com>
2020-05-26 16:38:08 +0100

    most of the functionalities are done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import babyNamesData from './babyNamesData.json';
-import ListBabyNames from "./components/ListBabyNames";
-import "./App.css";
+import ListBabyNames from './components/ListBabyNames';
+import image from './gender.png';
+import './App.css';
 const App = () => {
-  return  (
-      <div className="container">
-    <input className="search-input" placeholder="Search baby names..."></input> 
-    <hr className="horizontal-line"></hr>
-    <ListBabyNames babyNames = {babyNamesData}/>
-    <hr className="horizontal-line"></hr>
+  const [names, setName] = useState ('');
+  const [babyName, setBabyName] = useState ([]);
+
+  const handleChange = (e) => {
+    setName (e.target.value.toLowerCase ());
+  };
+
+  useEffect (
+    () => {
+      setBabyName (
+        babyNamesData.filter (item => item.name.toLowerCase ().includes (names))
+      );
+    },
+    [names]
+  );
+  return (
+    <div className="container">
+      <input
+        className="search-input"
+        placeholder="Search baby names..."
+        onChange={handleChange}
+      />
+      <img className="gender-image" src={image} alt="gender"/>
+      <hr className="horizontal-line"/>
+      <ListBabyNames babyNames={babyName} />
+      <hr className="horizontal-line" />
     </div>
   );
-}
+};
 export default App;
--- src/components/ListBabyNames.js
+++ src/components/ListBabyNames.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React from 'react';
 
-const ListBabyNames =(props)=>{
-    return (<div></div>);
-}
+const ListBabyNames = props => {
+  return (
+    <div className="all-Baby-Names">
+      {props.babyNames
+        .sort ((a, b) => a.name.localeCompare (b.name))
+        .map ((data, index) => (
+          <p key={index} className={data.sex}>{data.name}</p>
+        ))}
+    </div>
+  );
+};
 export default ListBabyNames;
