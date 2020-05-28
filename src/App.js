a076820e487f8a8f2d32db6f75628a0a3ddda7f8
byohannes <42332389+byohannes@users.noreply.github.com>
2020-05-28 19:15:11 +0100

    updated filtering by gender

--- src/App.js
+++ src/App.js
@@ -4,37 +4,38 @@ import ListBabyNames from './components/ListBabyNames';
 import image from './gender.png';
 import './App.css';
 const App = () => {
-  const [searchTerm, setSearchTerm] = useState ('');
+  const [searchTerm, setSearchTerm] = useState('');
 
-  const [genderOption, setGenderOption] = useState ('');
+  const [genderOption, setGenderOption] = useState('');
 
-  const filteredBabyNames = babyNamesData.filter (name =>
-    name.name.toLowerCase ().includes (searchTerm.toLowerCase ())
-  );
+  const filteredBabyNames = babyNamesData
+    .filter((babyInfo) => babyInfo.sex === genderOption || !genderOption)
+    .filter((babyInfo) =>
+      babyInfo.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
   return (
     <div className="container">
-
       <input
         className="search-input"
         value={searchTerm}
         placeholder="Search baby names..."
-        onChange={e => setSearchTerm (e.target.value)}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
-      <button className="search-button" onClick={() => setSearchTerm ('')}>
+      <button className="search-button" onClick={() => setSearchTerm('')}>
         Clear
       </button>
 
       <div className="radio-buttons">
-
         <span className="options">
           <input
             type="radio"
             name="gender"
             value="m"
-            onClick={e => {
-              setGenderOption (e.target.value);
+            onChange={(e) => {
+              setGenderOption(e.target.value);
             }}
+            checked={genderOption === 'm'}
           />
 
           <label className="gender">Male</label>
@@ -42,11 +43,22 @@ const App = () => {
             type="radio"
             name="gender"
             value="f"
-            onChange={e => {
-              setGenderOption (e.target.value);
+            onChange={(e) => {
+              setGenderOption(e.target.value);
             }}
+            checked={genderOption === 'f'}
           />
           <label className="gender">Female</label>
+          <input
+            type="radio"
+            name="gender"
+            value=""
+            onChange={(e) => {
+              setGenderOption('');
+            }}
+            checked={genderOption === ''}
+          />
+          <label className="gender">Male or Female</label>
         </span>
       </div>
 
